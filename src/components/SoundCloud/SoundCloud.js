f35e38b230780018814e232dc16d61427a3584a7
Hải Sơn Nguyễn <nguyensonhai1009@gmail.com>
2020-12-12 05:32:47 +0700

    update

--- src/components/SoundCloud/SoundCloud.js
+++ src/components/SoundCloud/SoundCloud.js
@@ -12,49 +12,55 @@ function SoundCloud() {
       </div>
       <div className={"soundclound"}>
         {/* Bloom */}
-        <ReactPlayer
-          url="https://soundcloud.com/khuongquach/bloom&show_artwork=false&color=%23EBA431"
-          width={window.innerWidth >= 768 ? 400 : "90%"}
-          height={window.innerWidth >= 768 ? 400 : 100}
-          className={"soundclound-player"}
-        />
-        {/* Checkpoint */}
-        <ReactPlayer
-          url="https://soundcloud.com/khuongquach/checkpoint&amp;show_artwork=false&color=%23EB8B15"
-          width={window.innerWidth >= 768 ? 400 : "90%"}
-          height={window.innerWidth >= 768 ? 400 : 100}
-          className={"soundclound-player"}
-        />
+        <div className={"soundclound-player"}>
+          <ReactPlayer
+            url="https://soundcloud.com/khuongquach/bloom&show_artwork=false&color=%23EBA431"
+            width={"100%"}
+            height={"100%"}
+          />
+        </div>
+        {/*Checkpoint */}
+        <div className={"soundclound-player"}>
+          <ReactPlayer
+            url="https://soundcloud.com/khuongquach/checkpoint&amp;show_artwork=false&color=%23EB8B15"
+            width={"100%"}
+            height={"100%"}
+          />
+        </div>
         {/* Horizon */}
-        <ReactPlayer
-          url="https://soundcloud.com/khuongquach/horizon-1&amp;show_artwork=false&color=%2397C4D0"
-          width={window.innerWidth >= 768 ? 400 : "90%"}
-          height={window.innerWidth >= 768 ? 400 : 100}
-          className={"soundclound-player"}
-        />
+        <div className={"soundclound-player"}>
+          <ReactPlayer
+            url="https://soundcloud.com/khuongquach/horizon-1&amp;show_artwork=false&color=%2397C4D0"
+            width={"100%"}
+            height={"100%"}
+          />
+        </div>
       </div>
       <div className={"soundclound"}>
         {/* Breeze */}
-        <ReactPlayer
-          url="https://soundcloud.com/khuongquach/swan-breeze&show_artwork=false&color=%2333393E"
-          width={window.innerWidth >= 768 ? 400 : "90%"}
-          height={window.innerWidth >= 768 ? 400 : 100}
-          className={"soundclound-player"}
-        />
+        <div className={"soundclound-player"}>
+          <ReactPlayer
+            url="https://soundcloud.com/khuongquach/swan-breeze&show_artwork=false&color=%2333393E"
+            width={"100%"}
+            height={"100%"}
+          />
+        </div>
         {/* Sunset */}
-        <ReactPlayer
-          url="https://soundcloud.com/khuongquach/swan-sunset&amp;show_artwork=false&color=%23BC4F64"
-          width={window.innerWidth >= 768 ? 400 : "90%"}
-          height={window.innerWidth >= 768 ? 400 : 100}
-          className={"soundclound-player"}
-        />
+        <div className={"soundclound-player"}>
+          <ReactPlayer
+            url="https://soundcloud.com/khuongquach/swan-sunset&amp;show_artwork=false&color=%23BC4F64"
+            width={"100%"}
+            height={"100%"}
+          />
+        </div>
         {/* Lights */}
-        <ReactPlayer
-          url="https://soundcloud.com/khuongquach/swan-lights&amp;show_artwork=false&color=%23C07442"
-          width={window.innerWidth >= 768 ? 400 : "90%"}
-          height={window.innerWidth >= 768 ? 400 : 100}
-          className={"soundclound-player"}
-        />
+        <div className={"soundclound-player"}>
+          <ReactPlayer
+            url="https://soundcloud.com/khuongquach/swan-lights&amp;show_artwork=false&color=%23C07442"
+            width={"100%"}
+            height={"100%"}
+          />
+        </div>
       </div>
     </div>
   );
